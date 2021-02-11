50961704cebad6d6dbde55180ad005b46a697bd3
Prikesh Savla <prikeshsavla@gmail.com>
2021-02-11 23:34:45 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module taco-bot
+
+go 1.14
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/nlopes/slack v0.6.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rapidloop/mybot v0.0.0-20160205033900-2777401b233f // indirect
+	github.com/slack-go/slack v0.8.0
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
+
+// You more than likely want your "Bot User OAuth Access Token" which starts with "xoxb-"
+var api = slack.New("xoxb-59268893569-1719504867733-4kFL22NM2iGqFSSldf13khxZ")
+
+func main() {
+	signingSecret := "44d0ac27e7e8b90214e65526a03d83f8"
+
+	http.HandleFunc("/events-endpoint", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Println("failed")
+			return
+		}
+		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println("failed")
+			return
+		}
+
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Println("failed")
+				return
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+			fmt.Println("challenged")
+		}
+		sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Println("failed")
+			return
+		}
+		if _, err := sv.Write(body); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println("failed")
+			return
+		}
+		if err := sv.Ensure(); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Println("failed")
+			return
+		}
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				{
+					api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+				}
+			case *slackevents.MessageEvent:
+				{
+					fmt.Println("Message", ev.Text)
+					fmt.Println("Message User", ev.User)
+
+					if strings.Contains(ev.Text, ":taco:") {
+						api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, Here is a taco for ", false))
+					}
+
+				}
+			case *slackevents.ReactionAddedEvent:
+				{
+					fmt.Println("Reaction", ev.Reaction)
+					if strings.Contains(ev.Reaction, "taco") {
+						api.PostMessage(ev.Item.Channel, slack.MsgOptionText("Yes, Here is a taco for ", false))
+					}
+				}
+			}
+		}
+	})
+	fmt.Println("[INFO] Server listening")
+	http.ListenAndServe(":3000", nil)
+}
